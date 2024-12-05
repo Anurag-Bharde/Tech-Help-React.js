a5019bdb16c04fa1c50ab0c1382550de60e6a7e7
Anurag-Bharde <anwolfragspitter99@gmail.com>
2024-12-05 18:49:46 +0530

    Adder

---
+++ src/Type/types.ts
@@ -0,0 +1,36 @@
+interface BloodPressure {
+    value: number;
+    levels: string;
+  }
+  
+  interface DiagnosisRecord {
+    month: string;
+    year: number;
+    blood_pressure: {
+      systolic: BloodPressure;
+      diastolic: BloodPressure;
+    };
+    heart_rate: BloodPressure;
+    respiratory_rate: BloodPressure;
+    temperature: BloodPressure;
+  }
+  
+  interface Diagnostic {
+    name: string;
+    description: string;
+    status: string;
+  }
+  
+  interface Patient {
+    name: string;
+    gender: string;
+    age: number;
+    profile_picture: string;
+    date_of_birth: string;
+    phone_number: string;
+    emergency_contact: string;
+    insurance_type: string;
+    diagnosis_history: DiagnosisRecord[];
+    diagnostic_list: Diagnostic[];
+    lab_results: string[];
+  }
