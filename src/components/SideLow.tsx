632d8e89e6f291cb7d98d92a316f20c6d5ff3724
Anurag-Bharde <anwolfragspitter99@gmail.com>
2024-12-07 23:38:09 +0530

    Overall DOne

--- src/components/SideLow.tsx
+++ src/components/SideLow.tsx
@@ -1,7 +1,24 @@
 import './Styles/SideLow.css'
+import { usePatient } from './Context/DataCont'
+import imgg from '../Images/download.svg'
 
 const SideLow = () => {
-    return <div id='sidelow'>SideLow</div>
+    const {selectedPatient}=usePatient();
+
+
+    return <div id='sidelow'>
+        <div>
+            <div>Lab Results</div>
+        </div>
+        <div>
+            {selectedPatient?.lab_results.map((items,index)=>(
+                <div key={index} className='flex justify-between'>
+                    <div>{items}</div>
+                    <img src={imgg} alt='downloads' />
+                </div>
+            ))}
+        </div>
+    </div>
 }
 
 export default SideLow
